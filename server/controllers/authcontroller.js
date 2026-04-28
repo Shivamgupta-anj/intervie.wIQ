e5427e8611547eabb383b392d51c20bdbd5d951d
Shivam gupta <guptashivam9161@gmail.com>
2026-04-28 14:09:04 +0530

    some changes

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -161,9 +161,9 @@ export const googleAuth = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-      // sameSite:"none",
+      secure: true,
+      // sameSite: "lax",
+      sameSite:"none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
