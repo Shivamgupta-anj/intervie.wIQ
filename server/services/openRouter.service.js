d23c0159733c538e849301bac50046cf3547686b
Shivam Gupta <guptashivam9161@gmail.com>
2026-07-06 15:15:16 +0530

    Change model in openRouter service to LLaMA 3.1

--- server/services/openRouter.service.js
+++ server/services/openRouter.service.js
@@ -8,7 +8,7 @@ export const askAi = async (messages)=>{
         throw new Error("Messages array is empty.");
     }
     const response = await axios.post("https://openrouter.ai/api/v1/chat/completions",{
-        model :"openai/gpt-4o-mini",
+        model: "meta-llama/llama-3.1-8b-instruct:free",
         messages: messages
     },{
      headers: {
